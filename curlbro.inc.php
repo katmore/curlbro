7c5f51efa909af922933846b97da3002ef720e9b
Doug Bird <doug@katmore.com>
2013-10-14 14:05:53 -0700

    stuff for handling posts

--- curlbro.inc.php
+++ curlbro.inc.php
@@ -18,9 +18,65 @@ class curlbro {
    private $url;
    private $cookiefile;
    private $usedom;
+   private $method;
+   private $postdata;
+   private $postcount;
+   private $postmtype;
+
+   public function setPOST($options="") {
+      $this->method = "POST";
+      $this->postcount=0;
+      $this->postdata='';
+      $this->postmtype='';
+      if (isset($options['Content-Type'])) {
+         if (isset($options['data'])) {
+            $this->postdata = $options['data'];
+            $this->postmtype=$options['Content-Type'];
+         }
+      } else
+      if (isset($options['fields'])) {
+         if (is_array($options['fields'])) {
+            
+            foreach($options['fields'] as $key=>$value) {
+               $this->postdata .= $key.'='.urlencode($value).'&';
+               $this->postcount++;
+            }
+            rtrim($fields_string, '&');
+            
+         }
+      }
+   }
+
+   public function __construct($config) {
+      /*
+       * default values
+       */
+      $this->useragent = 'curlbro';
+      $this->usedom = true;
+      $this->method = "GET";
+      
+      /*
+       * apply configuration
+       */
+      if (!empty($config['useragent'])) {
+         $this->useragent = $config['useragent'];
+      }
+      if (isset($config['usedom'])) {
+         if ($config['usedom']===false) {
+            $this->usedom = false;
+         }
+      }
+      if (!empty($config['cookiefile'])) {
+         $this->cookiefile = $config['cookiefile'];
+      } else {
+         $this->cookiefile = tempnam ( sys_get_temp_dir(),"curlbro");
+      }
+      
+      $this->config = $config;
+   }
 
    public function simplexpath($xquery,$demand_result=true) {
-      if (!$this->usedom) throw Exception("xpath query impossible: dom not loaded");
+      if (!$this->usedom) throw new Exception("xpath query impossible: dom not loaded");
       if (false === ($result =   $this->simplexml->xpath($xquery))) {
          throw new Exception(
          'xpath query issue'.
@@ -38,17 +94,9 @@ class curlbro {
       return $result;
    }
    public function __destruct() {
-      unlink($this->cookiefile);
+      @unlink($this->cookiefile);
    }
-   public function __construct($useragent='',$usedom=true) {
-      if ($useragent=='') {
-         $this->useragent = 'curlbro http://github.com/katmore/curlbro';
-      } else {
-         $this->useragent = $useragent;
-      }
-      $this->usedom = $usedom;
-      $this->cookiefile = tempnam ( sys_get_temp_dir(),"curl");
-   }
+
    public function __get($what) {
       if ($what=='info') return $this->info;
       if ($what=='document') return $this->document;
@@ -56,6 +104,9 @@ class curlbro {
       if ($what=='url') return $this->url;
       throw new Exception("call to unavailable property: '$what'");
    }
+   
+
+   
    public function curlexec($url,$referrer='',$savecookies=false,$usecookies=false) {
       $this->url = $url;
       $this->info = array();
@@ -73,6 +124,20 @@ class curlbro {
       if ($usecookies) {
          curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookiefile);
       }
+      if ($this->method=="POST") {
+         curl_setopt($ch, CURLOPT_POSTFIELDS, $this->postdata);
+         if ($this->postcount>0) {
+            curl_setopt($ch, CURLOPT_POST, $this->postcount);
+         }
+         if (!empty($this->postmtype)) {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+                'Content-Type: '.$this->postmtype,                                                                                
+                'Content-Length: ' . strlen($this->postdata))                                                                       
+            );
+         } 
+      }
+      
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       if (false === ($this->document = curl_exec($ch))) {
          throw new Exception('issue with curl_exec');
@@ -84,8 +149,11 @@ class curlbro {
          throw new Exception('could not get curlinfo');
       }
       if ($this->info['http_code']!='200') {
-         throw new Exception('did not get 200/ok response');
+         throw new Exception('did not get 200/ok response for '.$url);
       } 
+      if ($this->document=='') {
+         throw new Exception('empty document');
+      }
       curl_close($ch);
       
       
